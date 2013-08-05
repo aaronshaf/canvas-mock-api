1bff18f0f4f9ec7ebcf50869f2f236126592a049
Aaron Shafovaloff <ashafovaloff@instructure.com>
2013-08-05 12:27:07 -0600

    parse body

--- standalone.js
+++ standalone.js
@@ -4,6 +4,9 @@ var port = 3001;
 app.get('/', function(req, res) {
 	res.send(':-)');
 });
+
+app.use(express.bodyParser());
+  
 var middleware = require('./index');
 middleware(app);
 console.log('Listening to port ' + port);
